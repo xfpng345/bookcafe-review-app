4f4423e3a994924c43e4e24393c18304c9df53a2
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-06 15:09:46 +0900

    Update cafes_controller.rb

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,11 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module BookcafeReviewApp
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+  class Application < Rails::Application config.generators do |g|
+    g.stylesheets false
+    g.javascripts false
+    g.helper false
+    g.test_framework false
+  end
   end
 end
