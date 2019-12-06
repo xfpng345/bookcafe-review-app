3221db69bab7e0e133e6d939cbbf943f283221a1
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-06 15:14:18 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,11 @@ Bundler.require(*Rails.groups)
 
 module BookcafeReviewApp
   class Application < Rails::Application config.generators do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    g.test_framework false
-  end
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
