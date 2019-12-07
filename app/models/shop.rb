f40e5dd2e402b38dc7797d049bbc1ed828a3289a
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-07 23:54:25 +0900

    Update shop.rb

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,8 @@
 class Shop < ApplicationRecord
   mount_uploader :image, ImageUploader
+  def self.search(search)
+    return Shop.all unless search
+    Shop.where(['name LIKE ?', "%#{search}%"])
+    Shop.where(['text LIKE ?', "%#{search}%"])
+  end
 end
