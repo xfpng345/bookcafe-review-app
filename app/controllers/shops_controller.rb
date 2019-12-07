7c379a05d5f8cb6314f7cfb6ab6d0cb06c5849aa
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-07 11:37:58 +0900

    Update shops_controller.rb

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -13,5 +13,17 @@ class ShopsController < ApplicationController
   end
 
   def new
+    @shop = Shop.new
+   end
+
+   def create
+    @shop = Shop.create(shop_params)
+    redirect_to root_path
   end
+
+  private
+  def shop_params
+    params.require(:shop).permit(:name, :text, :image)
+  end
+  
 end
