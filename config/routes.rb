e8c9b3f9c5473fb7f9f3be97998a1caca1b83404
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-07 10:36:57 +0900

    Update index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root 'shops#index'
+
+  get 'shops/index'
+
+  get 'shops/show'
+
+  get 'shops/search'
+
+  get 'shops/edit'
+
+  get 'shops/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
