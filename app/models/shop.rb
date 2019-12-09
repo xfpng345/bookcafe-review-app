c24c0fc3b8b43f285754d4d31b4278950b90864f
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-09 17:35:05 +0900

    Update shop.rb

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,9 @@
 class Shop < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
+
   mount_uploader :image, ImageUploader
+  
   def self.search(search)
     return Shop.all unless search
     Shop.where(['name LIKE ?', "%#{search}%"])
