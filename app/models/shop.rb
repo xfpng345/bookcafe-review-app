4380feb4bb37dfd425a78d691b156ea0abc0b5ea
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-09 17:25:53 +0900

    Update shop.rb

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode
   mount_uploader :image, ImageUploader
   def self.search(search)
     return Shop.all unless search
