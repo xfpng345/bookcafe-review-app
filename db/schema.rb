d131fb740d89f2987acaab98719a33034e8cc7c4
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-09 15:56:27 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191209054953) do
+ActiveRecord::Schema.define(version: 20191209065237) do
 
   create_table "shops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20191209054953) do
     t.datetime "updated_at",               null: false
     t.string   "address"
     t.float    "latitude",   limit: 24
-    t.float    "langitude",  limit: 24
+    t.float    "longitude",  limit: 24
   end
 
 end
