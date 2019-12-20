8ee1c3d9bae0b4df5f8d9d6205b77207053d3968
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-20 14:36:45 +0900

    usernameカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191220052918) do
+ActiveRecord::Schema.define(version: 20191220053553) do
 
   create_table "shops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                     null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20191220052918) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
