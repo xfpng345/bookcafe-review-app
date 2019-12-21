1f8c6f2711d3cf4c01fdb9d7c76a8d860bdbf381
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-21 19:45:19 +0900

    userにimageカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191220104516) do
+ActiveRecord::Schema.define(version: 20191221104428) do
 
   create_table "shops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                     null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20191220104516) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
