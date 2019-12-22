4adfaf25652d65811f40164ba59a2afd43ef2a25
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-22 12:44:49 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :shops
+  has_many :shops, dependent: :destroy
 
   mount_uploader :image, ImageUploader
 end
