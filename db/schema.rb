3027c96440774e324479bc6c1031eb0d4c0fb000
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-28 14:09:39 +0900

    shops imageカラムのnot nullを外す

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191221104428) do
+ActiveRecord::Schema.define(version: 20191228050656) do
 
   create_table "shops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                     null: false
-    t.string   "image",                    null: false
+    t.string   "image"
     t.text     "text",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
