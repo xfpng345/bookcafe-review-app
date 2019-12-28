aed3c636b154c6672d8ec5057c0651395676f5f7
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-28 14:09:39 +0900

    shops imageカラムのnot nullを外す

---
+++ db/migrate/20191228050656_change_column_to_allow_null.rb
@@ -0,0 +1,9 @@
+class ChangeColumnToAllowNull < ActiveRecord::Migration[5.0]
+  def up
+    change_column :shops, :image,:string, null: true # null: trueを明示する必要がある
+  end
+
+  def down
+    change_column :shops, :image,:string, null: false
+  end
+end
