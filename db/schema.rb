53b6e7fa6a410a7ee78e6a2f6f93978addbd7620
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-28 16:46:06 +0900

    不要なカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191228050656) do
+ActiveRecord::Schema.define(version: 20191228073932) do
 
   create_table "shops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                     null: false
