96ef5b6062c4c74ab45e03033f27cf8d13d7d110
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-28 18:50:59 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = current_user
-    @shops = current_user.shops.page(params[:page]).per(5).order("created_at DESC")
+    @user = User.find_by(id: params[:id])
+    @shops = @user.shops.page(params[:page]).per(5).order("created_at DESC")
   end
 end
