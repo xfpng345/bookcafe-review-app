172ae313592fe9de74c0ded61a9666fd2102f20d
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-30 18:57:13 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-
   devise_for :users
   root 'shops#index'
-
-  resources :shops
-
   resources :users, only: [:show]
+  resources :shops do
+    resources :comments, only: [:create]
+  end
 end
