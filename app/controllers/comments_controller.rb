41f050164a6e6b37a3837b4864db3c04cb14e2c2
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2019-12-31 10:29:30 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,15 @@
 class CommentsController < ApplicationController
   def create
+    @shop = Shop.find(params[:shop_id])
+    @comment = @shop.comments.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to shop_path(@shop) } 
+      format.json
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id)
   end
 end
