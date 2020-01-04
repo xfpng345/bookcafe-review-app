8d237a8eadeaa372bb7dcceff6fe5c7710d84a94
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-04 13:07:14 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,23 @@
 class LikesController < ApplicationController
+  before_action :set_shop
+
   def create
+    @like = Like.create(user_id: current_user.id, shop_id: params[:shop_id])
+    @likes = Like.where(shop_id: params[:shop_id])
+    @shop.reload
   end
 
   def destroy
+    like = Like.find_by(user_id: current_user.id, shop_id: params[:shop_id])
+    like.destroy
+    @likes = Like.where(shop_id: params[:shop_id])
+    @shop.reload
   end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
+  end
+  
 end
