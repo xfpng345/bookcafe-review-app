c27907193d4394782349f86ab650bfce11114a10
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-05 23:16:17 +0900

    いいねしたshopを@liked_shopsに代入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
     @shops = @user.shops.page(params[:page]).per(5).order("created_at DESC")
+    @liked_shops = @user.like_shops.page(params[:page]).per(6).order("created_at DESC")
   end
 end
