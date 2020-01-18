6eacf423cffcfd62b740154e4d7276614630b773
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-18 14:08:31 +0900

    ルートパスにトップページを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'top#index'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions' }
-  root 'shops#index'
+    
   resources :users, only: [:show]
   resources :shops do
     resources :comments, only: [:create, :destroy]
