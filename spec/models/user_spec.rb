1faadd3b9ba4fba9a5ba077cfd123832b607ac5a
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-22 17:19:56 +0900

    テストにコメントアウトを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,29 +2,35 @@ require 'rails_helper'
 
 describe User do
   describe '#create' do
+
+    # 1. nicknameとemail、passwordとpassword_confirmationが存在すれば登録できること
     it "is valid with a nickname, email, password, password_confirmation" do
       user = build(:user)
       expect(user).to be_valid
     end
 
+    # 2. nicknameが空では登録できないこと
     it "is invalid without a username" do
       user = build(:user, username: "")
       user.valid?
       expect(user.errors[:username]).to include("を入力してください")
     end
 
+    # 3. emailが空では登録できないこと
     it "is invalid without an email" do
       user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("が入力されていません。")
     end
 
+    # 4. passwordが空では登録できないこと
     it "is invalid without a password" do
       user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("が入力されていません。")
     end
-
+    
+    # 5. passwordが存在してもpassword_confirmationが空では登録できないこと
     it "is invalid without a password_confirmation although with a password" do
       user = build(:user, password_confirmation: "")
       user.valid?
