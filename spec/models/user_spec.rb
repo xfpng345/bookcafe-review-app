2f833968b7bcde74f9cd9c314b91bda05cdcbc9b
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-22 17:14:08 +0900

    テストコードを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe User do
   describe '#create' do
+    it "is valid with a nickname, email, password, password_confirmation" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
 
     it "is invalid without a username" do
       user = build(:user, username: "")
