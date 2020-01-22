723d145cb79708cd6204bf02ab325e0445663a12
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-22 17:27:10 +0900

    テストコードを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,12 +29,46 @@ describe User do
       user.valid?
       expect(user.errors[:password]).to include("が入力されていません。")
     end
-    
+
     # 5. passwordが存在してもpassword_confirmationが空では登録できないこと
     it "is invalid without a password_confirmation although with a password" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+    # 7. nicknameが10文字以下では登録できること
+    it "is valid with a nickname that has less than 10 characters " do
+      user = build(:user, username: "1234567890")
+      expect(user).to be_valid
+    end
+
+    # 8. 重複したemailが存在する場合登録できないこと
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("は既に使用されています。")
+    end
+
+    # 9. passwordが6文字以上であれば登録できること
+    it "is valid with a password that has more than 6 characters " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+        # 9. passwordが6文字以上であれば登録できること
+    it "is valid with a password that has more than 6 characters " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    # 10. passwordが5文字以下であれば登録できないこと
+    it "is invalid with a password that has less than 5 characters " do
+      user = build(:user, password: "00000", password_confirmation: "00000")
+      user.valid?
+      expect(user.errors[:password]).to include("は6文字以上に設定して下さい。")
     end
 
   end
