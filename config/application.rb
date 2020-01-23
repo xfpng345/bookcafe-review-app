d72a5073da63f06f3bce42978ec5de47d070634c
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-23 20:42:40 +0900

    ja.ymlの読み込みを元に戻す

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 module BookcafeReviewApp
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
