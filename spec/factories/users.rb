4786dd0d6d019fee9805c213434424d047b36702
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-23 13:16:22 +0900

    fakerでemailをダミー生成する

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
 
   factory :user do
     username              {"abe"}
-    email                 {"kkk@gmail.com"}
     password              {"00000000"}
     password_confirmation {"00000000"}
     image                 {""}
+    sequence(:email) {Faker::Internet.email}
   end
 
 end
