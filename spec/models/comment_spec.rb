d4d1775656845eeeede70d759a58042ea1839da2
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-24 20:12:29 +0900

    テストコードを追加

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe '#create' do
+
+    context 'can save' do
+      it 'is valid with text' do
+        expect(build(:comment)).to be_valid
+      end
+    end
+
+    context 'can not save' do
+      it 'is invalid without text' do
+        comment = build(:comment, text: nil)
+        comment.valid?
+        expect(comment.errors[:text]).to include("を入力してください")
+      end
+
+      it 'is invalid without shop_id' do
+        comment = build(:comment, shop_id: nil)
+        comment.valid?
+        expect(comment.errors[:shop]).to include("を入力してください")
+      end
+
+      it 'is invaid without user_id' do
+        comment = build(:comment, user_id: nil)
+        comment.valid?
+        expect(comment.errors[:user]).to include("を入力してください")
+      end
+    end
+
+  end
+end
