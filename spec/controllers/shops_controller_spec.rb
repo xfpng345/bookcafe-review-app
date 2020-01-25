8e151bdd448d9349a7965d88b9411b2cbb4fe90d
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-25 19:33:01 +0900

    テストコードを編集
    ログインしているuser,idとshopのuser_idを同じにする

--- spec/controllers/shops_controller_spec.rb
+++ spec/controllers/shops_controller_spec.rb
@@ -19,13 +19,13 @@ describe ShopsController do
 
       describe 'GET #edit' do
         it "assigns the requested shop to @shop" do
-          shop = create(:shop)
+          shop = create(:shop, user_id: user.id)
           get :edit, params: { id: shop }
           expect(assigns(:shop)).to eq shop
         end
 
         it "renders the :edit template" do
-          shop = create(:shop)
+          shop = create(:shop, user_id: user.id)
           get :edit, params: { id: shop }
           expect(response).to render_template :edit
         end
