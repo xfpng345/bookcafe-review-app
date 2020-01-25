8912bd2aee2f7d086d59b2e731745593877bd9da
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-25 19:10:32 +0900

    ログインしている時としていない時を分ける

--- spec/controllers/shops_controller_spec.rb
+++ spec/controllers/shops_controller_spec.rb
@@ -32,18 +32,22 @@ describe ShopsController do
 
   end
 
-  describe 'GET #index' do
-    it "populates an array of shops ordered by created_at DESC" do
-      shops = create_list(:shop, 3) 
-      get :index
-      expect(assigns(:shops)).to match(shops)
-      # expect(assigns(:shops)).to match(shops.sort{ |a, b| b.created_at <=> a.created_at } )
+  context 'not log in' do
+
+    describe 'GET #index' do
+      it "populates an array of shops ordered by created_at DESC" do
+        shops = create_list(:shop, 3) 
+        get :index
+        expect(assigns(:shops)).to match(shops)
+        # expect(assigns(:shops)).to match(shops.sort{ |a, b| b.created_at <=> a.created_at } )
+      end
+
+      it "renders the :index template" do
+        get :index
+        expect(response).to render_template :index
+      end
     end
 
-    it "renders the :index template" do
-      get :index
-      expect(response).to render_template :index
-    end
   end
 
 end
