183f9018af55389bb5bbb9206430a9908c4a6345
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-25 14:31:12 +0900

    login userを追加しネストさせたテストをパスさせる

--- spec/controllers/shops_controller_spec.rb
+++ spec/controllers/shops_controller_spec.rb
@@ -1,25 +1,35 @@
 require 'rails_helper'
 
 describe ShopsController do
-  describe 'GET #new' do
-    it "renders the :new template" do
-      get :new
-      expect(response).to render_template :new
-    end
-  end
+  let(:user) { create(:user) }
 
-  describe 'GET #edit' do
-    it "assigns the requested shop to @shop" do
-      shop = create(:shop)
-      get :edit, params: { id: shop }
-      expect(assigns(:shop)).to eq shop
+
+  context 'log in' do
+    before do
+      login user
     end
 
-    it "renders the :edit template" do
-      shop = create(:shop)
-      get :edit, params: { id: shop }
-      expect(response).to render_template :edit
+    describe 'GET #new' do
+      it "renders the :new template" do
+        get :new
+        expect(response).to render_template :new
+      end
     end
+
+    describe 'GET #edit' do
+      it "assigns the requested shop to @shop" do
+        shop = create(:shop)
+        get :edit, params: { id: shop }
+        expect(assigns(:shop)).to eq shop
+      end
+
+      it "renders the :edit template" do
+        shop = create(:shop)
+        get :edit, params: { id: shop }
+        expect(response).to render_template :edit
+      end
+    end
+
   end
 
   describe 'GET #index' do
