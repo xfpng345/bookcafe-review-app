02967aea56b4dca7a35b8785174c9dbe0d1fe743
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-26 21:09:07 +0900

    テストコードを追加

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe CommentsController do
+  describe 'POST #create' do
+    let(:user) { create(:user) }
+    let(:shop) { create(:shop) }
+    let(:params) { { shop_id: shop.id, user_id: user.id, comment: attributes_for(:comment) } }
+
+    context 'log in' do
+      before do
+        login user
+      end
+      subject {
+         post :create,
+         params: params
+       }
+      it 'count up shop' do
+        expect{ subject }.to change(Comment, :count).by(1)
+      end
+    end
+
+  end
+end
