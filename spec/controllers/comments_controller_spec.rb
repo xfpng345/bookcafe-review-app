53ae7e63401e24835b4f29d4bbda2376f2fefbee
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-27 14:21:45 +0900

    letの定義位置を変更

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe CommentsController do
-  describe 'POST #create' do
-    let(:user) { create(:user) }
-    let(:shop) { create(:shop) }
-    let(:params) { { shop_id: shop.id, user_id: user.id, comment: attributes_for(:comment) } }
+  let(:user) { create(:user) }
+  let(:shop) { create(:shop) }
+  let(:params) { { shop_id: shop.id, user_id: user.id, comment: attributes_for(:comment) } }
 
+  describe 'POST #create' do
     context 'log in' do
       before do
         login user
@@ -18,6 +18,7 @@ describe CommentsController do
         expect{ subject }.to change(Comment, :count).by(1)
       end
     end
+  end
 
   end
 end
