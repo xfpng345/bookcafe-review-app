0bd220a23e42c9905c615ed373745ceb6f7c61b8
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-27 15:15:42 +0900

    テストコード追加
    render:show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 describe UsersController do
   describe 'GET #show' do
+
     it 'assigns @user' do
       user = create(:user)
       get :show, params: { id: user }
       expect(assigns(:user)).to eq user
     end
+
+    it "renders the :show template" do
+      user = create(:user)
+      get :show, params: { id: user }
+      expect(response).to render_template :show
+    end
+
   end
 end
