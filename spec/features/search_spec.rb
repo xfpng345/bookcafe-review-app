d163713cdbea25610f82d0cbec1f65bbe6517118
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-01-30 15:29:41 +0900

    テストコード追加
    コメントアウト部分でエラーが出ている。

---
+++ spec/features/search_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'search', type: :feature do
+  let(:user) { create(:user) }
+  let(:shop) { create(:shop) }
+
+  scenario 'search shop with shop.name' do
+    shop1 = create(:shop, name: 'aaa')
+    shop2 = create(:shop, name: 'bbb')
+    shop3 = create(:shop, name: 'ccc')
+
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_on 'ログインする'
+
+    visit shops_path
+    expect(page).to have_content 'カフェ一覧'
+    fill_in "q_name_or_user_username_cont", with: 'aaa'
+    find('input[name="commit"]').click
+    # expect(page).to have_no_content 'bbb'
+  end
+end
