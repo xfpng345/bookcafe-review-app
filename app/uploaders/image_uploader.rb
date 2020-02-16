1f2c2afd0d7ea20790070163e19d3bdb6f2837c2
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-02-16 15:41:00 +0900

    画像のアップロード先をS3に変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   process resize_to_fit: [800, 800]
 
-  storage :file
+  storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
