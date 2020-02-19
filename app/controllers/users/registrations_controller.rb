a3851dd703169e5cbf37c36cf7d0779e010af982
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-02-19 13:57:33 +0900

    Revert "testuserを編集する為一時的にコメントアウト"
    This reverts commit 6bf704608e1ddf1d41a30336a0692c0f62f11106.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :forbid_test_user, only: %i[edit update destroy]
+  before_action :forbid_test_user, only: %i[edit update destroy]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
