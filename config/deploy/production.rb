111c8c5fb86565b776bbb9c384810b5b35a4fd48
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-03-25 16:33:20 +0900

    デプロイキーを変更
    EC2のデフォルト鍵

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 server '54.248.37.95', user: 'test_user', roles: %w{app db web}
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/first_aws_rsa'],
+                  keys: ['~/.ssh/key_book_cafe_review_app.pem'],
                   forward_agent: true
