e5973e48f1d4c7001430762a6e015a105915bccf
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-03-26 14:20:52 +0900

    circleci上のssh鍵にパスを変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 server '54.248.37.95', user: 'test_user', roles: %w{app db web}
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/key_book_cafe_review_app.pem'],
+                  keys: ["~/.ssh/id_rsa_41cedb362f1967ff6fdfeeea3d5da3f3"],
+                  # keys: ["#{ENV.fetch('PRODUCTION_SSH_KEY')}"],
                   forward_agent: true
