22543540d7f98c8ce8e75b40377b154cf2f4998b
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-04-02 11:53:54 +0900

    getメソッドのコントローラを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
     @shops = @user.shops.page(params[:page]).per(6).order('created_at DESC')
     @liked_shops = @user.like_shops.page(params[:page]).per(6).order('created_at DESC')
   end
+
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
 end
