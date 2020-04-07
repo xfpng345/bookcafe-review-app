7e6f38a18e835fb95aec6ed3632408367e22de32
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-04-07 22:20:20 +0900

    地図表示ページのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,7 @@ Rails.application.routes.draw do
     resources :comments, only: %i[create destroy]
     resources :likes, only: %i[create destroy]
   end
+
+  get 'mapindex' => 'shops#mapindex'
+
 end
