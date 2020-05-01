782ee3ce9af2ebc6a7bb981af9397c8e1c5f799c
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-01 17:19:03 +0900

    不要なアソシエーションを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :like_shops, through: :likes, source: :shop
 
-  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy, inverse_of: :user
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :following, through: :following_relationships
-  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy, inverse_of: :user
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
   has_many :followers, through: :follower_relationships
 
   has_many :active_notifications, foreign_key: 'visitor_id', class_name: 'Notification', dependent: :destroy
