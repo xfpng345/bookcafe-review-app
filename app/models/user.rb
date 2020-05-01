5050393f083e4405d44ea1aff6c6cf479eba091d
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-01 17:58:53 +0900

    create_notification_follow!メソッドを作成
    フォロー時にnotificationレコードを生成する

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,15 @@ class User < ApplicationRecord
   def unfollow(user)
     following_relationships.find_by(following_id: user.id).destroy
   end
+  
+  def create_notification_follow!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+      )
+      notification.save if notification.valid?
+    end
+  end
 end
