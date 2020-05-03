5129127972dfc5b72fec733237a82a972e56eab0
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-03 21:50:53 +0900

    omniauthコールバックメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook twitter google_oauth2]
 
   has_many :shops, dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -45,5 +46,12 @@ class User < ApplicationRecord
       action: 'follow'
     )
     notification.save if notification.valid?
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
   end
 end
