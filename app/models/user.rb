7f9a0238b6e4ff8460089f54bed8b17a55e78be2
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-04 16:45:02 +0900

    omniauthコールバック時にnameを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,6 +51,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.username = auth.info.name
       user.password = Devise.friendly_token[0,20]
     end
   end
