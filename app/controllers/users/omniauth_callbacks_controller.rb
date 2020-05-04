46922cbff0558dae7c14a3b54433332b737ec917
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-04 17:54:17 +0900

    成功時のフラッシュメッセージを追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,6 +18,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
     else
       session["devise.user_attributes"] = @user.attributes  # 認証データを覚えておく。
       render template: "devise/registrations/new" 
