f653478237123cc7fad9730779803084dcc38bc1
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-04 17:32:00 +0900

    omniauth コールバックメソッドを作成

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,31 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+  def facebook
+    callback_for(:facebook)
+  end
 
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
+  def twitter
+    callback_for(:twitter)
+  end
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
+  def google_oauth2
+    callback_for(:google)
+  end
 
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
+  def callback_for(provider)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.user_attributes"] = @user.attributes  # 認証データを覚えておく。
+      render template: "devise/registrations/new" 
+    end
+  end
 
-  # protected
+  def failure
+    redirect_to root_path
+  end
 
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 end
