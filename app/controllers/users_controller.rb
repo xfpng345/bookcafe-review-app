dae4695121e5530c3582d72104052872c1f594b8
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-07 23:51:33 +0900

    ユーザー詳細ページを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
-    @shops = @user.shops.page(params[:page]).per(6).order('created_at DESC')
+    @shops = @user.shops.includes(:user, :likes).page(params[:page]).per(6).order('created_at DESC')
     @liked_shops = @user.like_shops.page(params[:page]).per(6).order('created_at DESC')
   end
 
