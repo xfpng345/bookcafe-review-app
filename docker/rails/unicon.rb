cac660bf01311c885816860233d4fd9958dc60d2
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-23 17:29:01 +0900

    timwoutを追加し、各ファイルのディレクトリを修正

--- docker/rails/unicon.rb
+++ docker/rails/unicon.rb
@@ -1,7 +1,8 @@
 worker_processes 8
+timeout 30
 
-pid "/var/run/unicorn.pid"
-listen "/var/tmp/unicorn.sock"
+pid '/var/www/projects/bookcafe-review-app/tmp/pids/unicorn.pid'
+listen '/var/www/projects/bookcafe-review-app/tmp/unicorn.sock'
 
-stdout_path "./log/unicorn.stdout.log"
-stderr_path "./log/unicorn.stderr.log"
+stdout_path "/var/www/projects/bookcafe-review-app/log/unicorn.stdout.log"
+stderr_path "/var/www/projects/bookcafe-review-app/log/unicorn.stderr.log"
